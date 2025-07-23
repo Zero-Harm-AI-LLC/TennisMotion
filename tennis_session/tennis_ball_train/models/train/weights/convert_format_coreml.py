6391dd57e023f191cca8bd6990621e6a4259e07b
Dzung Tran <dzung.d.tran@gmail.com>
2025-07-23 07:50:23 -0700

    Added compilation of CoreML model

--- tennis_session/tennis_ball_train/models/train/weights/convert_format_coreml.py
+++ tennis_session/tennis_ball_train/models/train/weights/convert_format_coreml.py
@@ -6,6 +6,7 @@
 #
 from ultralytics import YOLO
 import os
+import coremltools
 
 # === CONFIGURATION ===
 pt_model_path = 'best.pt'  # Replace with your trained model
@@ -15,4 +16,6 @@ model = YOLO(pt_model_path)
 
 # === EXPORT TO COREML ===
 coreml_output = model.export(format='coreml', dynamic=False, optimize=True)
+model_path = coremltools.utils.compile_model("best.mlpackage", "yolov8.mlmodelc")
 print(f"CoreML model saved at: {coreml_output}")
+print(f"Compiled CoreML model saved at: {model_path}")
