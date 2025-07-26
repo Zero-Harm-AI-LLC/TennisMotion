88775c7381d489f9b12de9977646862e0fe4377a
Dzung Tran <dzung.d.tran@gmail.com>
2025-07-25 18:37:13 -0700

    Revert to no nms and remove unused imports

--- tennis_session/tennis_ball_train/models/train/weights/convert_format_coreml.py
+++ tennis_session/tennis_ball_train/models/train/weights/convert_format_coreml.py
@@ -5,19 +5,14 @@
 # This script converts a YOLOv8 model to CoreML format.
 #
 from ultralytics import YOLO
-import os
-import coremltools
 import shutil
 
 # === CONFIGURATION ===
-pt_model_path = 'best.pt'  # Replace with your trained model
+pt_model_path = 'yolov8.pt'  # Replace with your trained model
+shutil.copy('best.pt', pt_model_path)  # Copy the model to the specified path
 
 # === LOAD THE MODEL ===
 model = YOLO(pt_model_path)
 
 # === EXPORT TO COREML ===
-coreml_output = model.export(format='coreml', nms=True, dynamic=False, optimize=True)
-shutil.move(coreml_output, 'yolov8.mlpackage')  # Move the exported model to a specific directory
-#model_path = coremltools.utils.compile_model("best.mlpackage", "yolov8.mlmodelc")
-print(f"CoreML model saved at: {coreml_output}")
-#print(f"Compiled CoreML model saved at: {model_path}")
+coreml_output = model.export(format='coreml', dynamic=False, optimize=True)
